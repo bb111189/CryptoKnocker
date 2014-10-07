72b7929555467908c467327fa80f03582353d335
Anselm <anselm@vista.aero>
2014-10-07 23:04:48 +0800

    UDP chatting + public private key encryption

---
+++ Client.py
@@ -0,0 +1,53 @@
+
+client_private_key = '-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEAzsH1AvD7mvqit5YvbAekHNmGIgsTNVaJpl5JCMb0RjCdKELd\ng49BNC1Ct2v90OYpaChyEEJzf/gUT1WgNsfIK1MV05JlLTOR4uObqTXKlXv6zVsN\nKsoPf64k+Apfq+ZuH7VpBTgTGFgzNDEqrow0r9yMCAbNrT8jH1NuG3GWhYBLcOyj\nj9tfj2BA89LywHHAjqk2bmtxj2oXBHaEZeo7xKzDZ8DtMzWA9yH0RnNnx84KmH6n\nLaGkc2pOjQ/d0Hw1AN8DlS5EtFB86Z6YbXr7ptHuSNSTkzKZOSuKCZi3DSlY9+CF\nveN5YV9cInf+E5pdSH4/njOJCkNAsxtKUPCKyQIDAQABAoIBAERn8ZtWhbhB4mIh\nQ3FXmdmdsn5C8cj856/uk/VedeVfIi/253kmhWkw7FjpVuGttNdsQkjU44fcWUPi\nTY6yTKzg4WDNl8H7xPGvYon6bRtGGLWmwVV94uDYigyyyfKC276L88IxSkgMrmw/\nNCLp6mig7IefUJ9YO1Oa89wKmcVzE70w2ONyKagqLmwnnXww8o5Hg1EMwxJdSQ/H\n32JeIm8skhQzgqS37F/2MJpbeaI7aEo8SyLltNW4sLLS/+JT6U/a0koiokb5NxnY\nujfBg6VC/+UC+/gsd0I/uPQYSBr8c3etr2+WVuVaLwUwnzFeL2n75oiLi5WawiwY\nLOkrQ30CgYEA4TXULv+Cq1I1fZz8PiNkteVK26TWZXyfPGjmnUWNExJS5eLUvJup\nlEhcaw4uZ/RvfrHU54mUwDK5I76EXCcQ4QAPqu29+7fuSmC+Gf5N8J7WDqKfV7aC\nN3FJwlxYfMKvGMvVs2OJQ0LgKpQgvvqnoKcb33ltS2ZwJrGe5oNVFF8CgYEA6wZN\nQ8mtnXXaFRhbu8I5deu7ZP5PHazl8d2bviezj6hKtvA2hng6pck/NPxi2KGv6R4Q\nY8i8JnVaxjKbxHiH8IM/FMdyZ8glWI0C36SmkwVa8AZTcmT0TK+bqKzStVjExJGE\nZ0ziQ3zadAu0zu+b9jVpBz9ThAE7xMnQcK4R8dcCgYEAtk23rC/DW+GZUUyR188Y\nyDGLLk+6Cb8eu20DOU5Cy6xqlJLQuLidRl5pCXQaVqZE1t8nRCJX2ll1Cx0+0cYB\n4NEG3cvDnY8fgu/bEvmLp/s7te8oY31m3XCET6QnMR77VGw0Whl51Iu/IFh2RYJr\nzR1Y78bAlWef44Km6Vxos6sCgYAH5KFJqodLJ38YgEXsa1x0qmMTxDqx+3GJivDF\nFR5Br9w6gpzCLV2C3dxikitbrSp0QM86Gq6+9+gBigeTNJ0UJcRDlWMxOxaN/8Zd\n7nCttVtVflDlIaDWxnsK0isNbhrFo0F4GqvqqlHH3fDHlmJ9hVOuq6DJaSUxubln\nX2E45wKBgQDggBOfNJBnhIIzoDJLJ0uQPNIpRXvRQRnCEFGlJsTdDGkaE7+8aany\nfPoJ4eHeD4tb0umlYvUNWgfsRm6vfehypa70eTTx8jnKYGhp1FRdC3M2uWoS1RdF\nTdJOEQpvDZm2ZmwFKshZlmsupqwCcKJkJ49IA/zdWCmjv87ml2TjOA==\n-----END RSA PRIVATE KEY-----'
+client_public_key = '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzsH1AvD7mvqit5YvbAek\nHNmGIgsTNVaJpl5JCMb0RjCdKELdg49BNC1Ct2v90OYpaChyEEJzf/gUT1WgNsfI\nK1MV05JlLTOR4uObqTXKlXv6zVsNKsoPf64k+Apfq+ZuH7VpBTgTGFgzNDEqrow0\nr9yMCAbNrT8jH1NuG3GWhYBLcOyjj9tfj2BA89LywHHAjqk2bmtxj2oXBHaEZeo7\nxKzDZ8DtMzWA9yH0RnNnx84KmH6nLaGkc2pOjQ/d0Hw1AN8DlS5EtFB86Z6YbXr7\nptHuSNSTkzKZOSuKCZi3DSlY9+CFveN5YV9cInf+E5pdSH4/njOJCkNAsxtKUPCK\nyQIDAQAB\n-----END PUBLIC KEY-----'
+server_public_key = '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzGp69YJK5LjvG2oAJiwa\nha55Lr6G9RpQYEiXZvBS3GzXYUUs4ThAvVymrIS5J5RGqWJjyduW3i2PfRBrT8h/\nH43Vy0rjs1rnCjU8ORc0Z+gKmiFDyi3wqEBgjPR8sx66/jHRY6cAraA0ngyAp5KN\nMAqeIfD7LvzVM9sakqxO0QIAdxPrCzlcUxx1tjxtOJ5hheOVTzhDBcsJWT9PscNi\nlP8w9EaSeg4+ZGRRlcFNx9EBe6M+Ov7mbuKoigfnXKgza+GXeJOYIGfdhh9XiFgd\nmfI59VPOxsaDzywlAL12eWZczaK2lC5cyk6IKg2zoWrPf13MtIG1iREKaxmOJbVp\nJwIDAQAB\n-----END PUBLIC KEY-----'
+
+
+import socket
+import sys
+from crypto import encrypt_RSA, decrypt_RSA
+
+HOST = ''    # Symbolic name meaning all available interfaces
+PORT = 8888    # Arbitrary non-privileged port
+
+# Datagram (udp) socket
+try :
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print 'Socket created'
+except socket.error, msg :
+    print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+
+
+# Bind socket to local host and port
+try:
+    s.bind((HOST, PORT))
+except socket.error , msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+    
+print 'Socket bind complete'
+
+#now keep talking with the client
+while 1:
+    # receive data from client (data, addr)
+    d = s.recvfrom(1024)
+    data = d[0]
+    addr = d[1]
+    
+    data = decrypt_RSA(client_private_key, data)
+    #data = decrypt_RSA(server_public_key, data)
+    
+    if not data: 
+        break
+    
+    reply = 'OK...' + data
+    
+    reply = encrypt_RSA(server_public_key, reply)
+    #reply = encrypt_RSA(client_private_key, reply)
+    
+    s.sendto(reply , addr)
+    print 'Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + data.strip()
+    
+s.close()
---
+++ Server.py
@@ -0,0 +1,40 @@
+
+server_private_key = '-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEAzGp69YJK5LjvG2oAJiwaha55Lr6G9RpQYEiXZvBS3GzXYUUs\n4ThAvVymrIS5J5RGqWJjyduW3i2PfRBrT8h/H43Vy0rjs1rnCjU8ORc0Z+gKmiFD\nyi3wqEBgjPR8sx66/jHRY6cAraA0ngyAp5KNMAqeIfD7LvzVM9sakqxO0QIAdxPr\nCzlcUxx1tjxtOJ5hheOVTzhDBcsJWT9PscNilP8w9EaSeg4+ZGRRlcFNx9EBe6M+\nOv7mbuKoigfnXKgza+GXeJOYIGfdhh9XiFgdmfI59VPOxsaDzywlAL12eWZczaK2\nlC5cyk6IKg2zoWrPf13MtIG1iREKaxmOJbVpJwIDAQABAoIBAF3sDtL/KqoszrAp\n5Cel3AnQQJ3ZrOixTnzu3aHOikP9PnjS7+wLjLN0/qAF24zz9AOf0XnOC2PKS0OL\nZ8MgvEgjQzhFEJo4CjAGhcU1hD65vyVSY3Nh6FFmQvaA30rts71X5n2DHiyoj4SO\n+g4EqBqKxl2R+9d7E/rfkTYTNaOSFT3ncxdIZ7VtPGCFtkTpgVfG36bL6xFJg5v4\nxfAkZfRBbwixtB25QrnoBGk5DR9GB7gl2VoDtchE5kKfqlBBv1TpT3fxwSi+Jh4K\nHOWaHJmBYcX1xkq5tyYTWTLaG1lkmtKH+6NzeBI4iwt41tH5ru/bTzszgIXzR5db\nNPg5twECgYEA1oNgdPqBhx7D1wdTpylmhyQKpFuSwSJNCEMFN+MHpkAyz6cYvVn7\nxnfgJzZ6YIs9QHOMC8nci4GU3lxC03Dj/wW54jDJxig+FYR780IQS0HKmsrNQe0v\neOcOtZAMxZ2braF7hD66TDMouzTRVK4fT8BIB2BntDK3aNv3rccXh0MCgYEA8/Mv\nXzlKv7sa/CbdsqdL7oDKmGtAJwwf+BAAIUDoKge+v+Od8dTfUax+MdfRNfCDGXaz\nNJ6XeZEfsIrCypqXulParjJJMt3Ht3GV1QrHH3CoEejC12M/bR5vmdPTHt+Z0LXJ\nfZg/3nwSx8E9ti/SbbW/idR8hidjlPRPv47qvk0CgYBvLfJdTsS5coGQPjTmK7f/\nDPgTDX83aH7CbHIU8C8zxIxRQcq5wRyUVecwi7TLuv8JG5oXNg1/pK6yvODJ0/Xn\nZGFeUWkSsiLTZjIT90aYqsXGipvtsm/rWt4tzjZAahqY2Fw7Q4OtIMrYlFx2ZjTG\nUeNDJ304URzsVS2+vLR+HwKBgBboPQiKDrTWUwBefe7quryYp22MlKeWAeJ/TiPR\nPnvD4muOaiRtQtMeiXMQuvp/rS/U75r/UWX+9KTVQtlXjuvWN/CnlCwMm1ZkehdZ\nPb18DKwx2Sn0jd4nP3+IXojFomHw0AzVIRo5OGI9iRf0gaP/rw64P0Exra4tCtgI\nCY75AoGBAIYq+0GmI3g2O/k2k4aMrs4sHklg8TaTGvkS5iXxzGbW6sge3RxSlqHl\nF0l0Oqa30zQ8ddsCuHsAPH2oeDVgC7amFNe+80dECkTZPBSuG5CEz7PPWJdDKw2d\nBS49ZBaHhfg4gKBy0Bc+/OT2jV1iff2M8PN6DR2Sl8Q79gS3e8Go\n-----END RSA PRIVATE KEY-----'
+server_public_key = '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzGp69YJK5LjvG2oAJiwa\nha55Lr6G9RpQYEiXZvBS3GzXYUUs4ThAvVymrIS5J5RGqWJjyduW3i2PfRBrT8h/\nH43Vy0rjs1rnCjU8ORc0Z+gKmiFDyi3wqEBgjPR8sx66/jHRY6cAraA0ngyAp5KN\nMAqeIfD7LvzVM9sakqxO0QIAdxPrCzlcUxx1tjxtOJ5hheOVTzhDBcsJWT9PscNi\nlP8w9EaSeg4+ZGRRlcFNx9EBe6M+Ov7mbuKoigfnXKgza+GXeJOYIGfdhh9XiFgd\nmfI59VPOxsaDzywlAL12eWZczaK2lC5cyk6IKg2zoWrPf13MtIG1iREKaxmOJbVp\nJwIDAQAB\n-----END PUBLIC KEY-----'
+client_public_key = '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzsH1AvD7mvqit5YvbAek\nHNmGIgsTNVaJpl5JCMb0RjCdKELdg49BNC1Ct2v90OYpaChyEEJzf/gUT1WgNsfI\nK1MV05JlLTOR4uObqTXKlXv6zVsNKsoPf64k+Apfq+ZuH7VpBTgTGFgzNDEqrow0\nr9yMCAbNrT8jH1NuG3GWhYBLcOyjj9tfj2BA89LywHHAjqk2bmtxj2oXBHaEZeo7\nxKzDZ8DtMzWA9yH0RnNnx84KmH6nLaGkc2pOjQ/d0Hw1AN8DlS5EtFB86Z6YbXr7\nptHuSNSTkzKZOSuKCZi3DSlY9+CFveN5YV9cInf+E5pdSH4/njOJCkNAsxtKUPCK\nyQIDAQAB\n-----END PUBLIC KEY-----'
+
+import socket 
+import sys
+from crypto import encrypt_RSA, decrypt_RSA
+
+# create dgram udp socket
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except socket.error:
+    print 'Failed to create socket'
+    sys.exit()
+
+host = 'localhost';
+port = 8888;
+
+while(1) :
+    msg = raw_input('Enter message to send : ')
+    #msg = encrypt_RSA(server_private_key, msg)
+    msg = encrypt_RSA(client_public_key, msg)
+    try :
+        #Set the whole string
+        s.sendto(msg, (host, port))
+        
+        # receive data from client (data, addr)
+        d = s.recvfrom(1024)
+        reply = d[0]
+        addr = d[1]
+        
+        reply = decrypt_RSA(server_private_key, reply)
+        #reply = decrypt_RSA(client_public_key, reply)
+        
+        print 'Server reply : ' + reply
+    
+    except socket.error, msg:
+        print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        sys.exit()
