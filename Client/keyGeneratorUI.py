b53ce1e22e58a3c8715d5f000b9997a205fe9253
Junhaaooooo <tanjunhao@outlook.com>
2014-11-02 14:58:19 +0800

    Added method to generate pub and priv key and stored them into folders. Added method to write path of keys to ini file

--- Client/keyGeneratorUI.py
+++ Client/keyGeneratorUI.py
@@ -2,6 +2,9 @@ __author__ = 'Junhaaooooo'
 import sys
 from PyQt4 import QtCore, QtGui
 from genPopUp import Ui_genPopup
+from Crypto.PublicKey import RSA
+import ConfigParser
+import os
 
 class KeyGeneratorUI(QtGui.QMainWindow):
     def setIconImage(self):
@@ -18,15 +21,46 @@ class KeyGeneratorUI(QtGui.QMainWindow):
         self.setWindowTitle("Generate new pair of key")
         self.ui = Ui_genPopup()
         self.ui.setupUi(self)
-
         self.ui.generateButton.clicked.connect(self.generateKey);
 
-    def generateKey(self):
-        print self.ui.lineEdit.text()
-        self.ui.label_2.setText("Keys generated")
-        print "generated"
+    def generateRSAKeyPair(self, userName):
+        privFileName = "./keys/private/" + userName + ".key"
+        pubFileName = "./keys/public/" + userName + ".key"
 
-        QtCore.QTimer.singleShot(3000, self.closeKeyGen)
+        rsakeys = RSA.generate(2048)
+        privHandle = open(privFileName, 'wb')
+        privHandle.write(rsakeys.exportKey())
+        privHandle.close()
+
+        pubHandle = open(pubFileName, 'wb')
+        pubHandle.write(rsakeys.publickey().exportKey())
+        pubHandle.close()
+
+        return os.path.abspath(privFileName), os.path.abspath(pubFileName)
+
+    def writeToINI(self, userName, result):
+        cfg = ConfigParser.ConfigParser()
+        cfg.read('user.ini')
+        if not cfg.has_section('User private key'):
+            cfg.add_section('User private key')
+        if not cfg.has_section('User public key'):
+            cfg.add_section('User public key')
+        cfg.set('User private key', str(userName), result[0])
+        cfg.set('User public key', str(userName), result[1])
+        f = open('user.ini', 'w')
+        cfg.write(f)
+        f.close()
+
+    def generateKey(self):
+        userName =  self.ui.lineEdit.text()
+        if (userName != ""):
+            result = self.generateRSAKeyPair(userName)
+            self.writeToINI(userName, result)
+            self.ui.label_2.setText("Keys generated")
+            QtCore.QTimer.singleShot(3000, self.closeKeyGen)
+
+        else:
+            self.ui.label_2.setText("Please enter a user name")
 
     def closeKeyGen(self):
         self.close()
