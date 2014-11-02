c8be7ff410c269b5f36732ecd1d9cc48eae31f17
Junhaaooooo <tanjunhao@outlook.com>
2014-11-03 01:41:38 +0800

    Adapted client communication from existing scripts [not tested]

---
+++ Client/clientComms.py
@@ -0,0 +1,74 @@
+# client_private_key = '-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEAzsH1AvD7mvqit5YvbAekHNmGIgsTNVaJpl5JCMb0RjCdKELd\ng49BNC1Ct2v90OYpaChyEEJzf/gUT1WgNsfIK1MV05JlLTOR4uObqTXKlXv6zVsN\nKsoPf64k+Apfq+ZuH7VpBTgTGFgzNDEqrow0r9yMCAbNrT8jH1NuG3GWhYBLcOyj\nj9tfj2BA89LywHHAjqk2bmtxj2oXBHaEZeo7xKzDZ8DtMzWA9yH0RnNnx84KmH6n\nLaGkc2pOjQ/d0Hw1AN8DlS5EtFB86Z6YbXr7ptHuSNSTkzKZOSuKCZi3DSlY9+CF\nveN5YV9cInf+E5pdSH4/njOJCkNAsxtKUPCKyQIDAQABAoIBAERn8ZtWhbhB4mIh\nQ3FXmdmdsn5C8cj856/uk/VedeVfIi/253kmhWkw7FjpVuGttNdsQkjU44fcWUPi\nTY6yTKzg4WDNl8H7xPGvYon6bRtGGLWmwVV94uDYigyyyfKC276L88IxSkgMrmw/\nNCLp6mig7IefUJ9YO1Oa89wKmcVzE70w2ONyKagqLmwnnXww8o5Hg1EMwxJdSQ/H\n32JeIm8skhQzgqS37F/2MJpbeaI7aEo8SyLltNW4sLLS/+JT6U/a0koiokb5NxnY\nujfBg6VC/+UC+/gsd0I/uPQYSBr8c3etr2+WVuVaLwUwnzFeL2n75oiLi5WawiwY\nLOkrQ30CgYEA4TXULv+Cq1I1fZz8PiNkteVK26TWZXyfPGjmnUWNExJS5eLUvJup\nlEhcaw4uZ/RvfrHU54mUwDK5I76EXCcQ4QAPqu29+7fuSmC+Gf5N8J7WDqKfV7aC\nN3FJwlxYfMKvGMvVs2OJQ0LgKpQgvvqnoKcb33ltS2ZwJrGe5oNVFF8CgYEA6wZN\nQ8mtnXXaFRhbu8I5deu7ZP5PHazl8d2bviezj6hKtvA2hng6pck/NPxi2KGv6R4Q\nY8i8JnVaxjKbxHiH8IM/FMdyZ8glWI0C36SmkwVa8AZTcmT0TK+bqKzStVjExJGE\nZ0ziQ3zadAu0zu+b9jVpBz9ThAE7xMnQcK4R8dcCgYEAtk23rC/DW+GZUUyR188Y\nyDGLLk+6Cb8eu20DOU5Cy6xqlJLQuLidRl5pCXQaVqZE1t8nRCJX2ll1Cx0+0cYB\n4NEG3cvDnY8fgu/bEvmLp/s7te8oY31m3XCET6QnMR77VGw0Whl51Iu/IFh2RYJr\nzR1Y78bAlWef44Km6Vxos6sCgYAH5KFJqodLJ38YgEXsa1x0qmMTxDqx+3GJivDF\nFR5Br9w6gpzCLV2C3dxikitbrSp0QM86Gq6+9+gBigeTNJ0UJcRDlWMxOxaN/8Zd\n7nCttVtVflDlIaDWxnsK0isNbhrFo0F4GqvqqlHH3fDHlmJ9hVOuq6DJaSUxubln\nX2E45wKBgQDggBOfNJBnhIIzoDJLJ0uQPNIpRXvRQRnCEFGlJsTdDGkaE7+8aany\nfPoJ4eHeD4tb0umlYvUNWgfsRm6vfehypa70eTTx8jnKYGhp1FRdC3M2uWoS1RdF\nTdJOEQpvDZm2ZmwFKshZlmsupqwCcKJkJ49IA/zdWCmjv87ml2TjOA==\n-----END RSA PRIVATE KEY-----'
+#client_public_key = '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzsH1AvD7mvqit5YvbAek\nHNmGIgsTNVaJpl5JCMb0RjCdKELdg49BNC1Ct2v90OYpaChyEEJzf/gUT1WgNsfI\nK1MV05JlLTOR4uObqTXKlXv6zVsNKsoPf64k+Apfq+ZuH7VpBTgTGFgzNDEqrow0\nr9yMCAbNrT8jH1NuG3GWhYBLcOyjj9tfj2BA89LywHHAjqk2bmtxj2oXBHaEZeo7\nxKzDZ8DtMzWA9yH0RnNnx84KmH6nLaGkc2pOjQ/d0Hw1AN8DlS5EtFB86Z6YbXr7\nptHuSNSTkzKZOSuKCZi3DSlY9+CFveN5YV9cInf+E5pdSH4/njOJCkNAsxtKUPCK\nyQIDAQAB\n-----END PUBLIC KEY-----'
+#server_public_key = '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzGp69YJK5LjvG2oAJiwa\nha55Lr6G9RpQYEiXZvBS3GzXYUUs4ThAvVymrIS5J5RGqWJjyduW3i2PfRBrT8h/\nH43Vy0rjs1rnCjU8ORc0Z+gKmiFDyi3wqEBgjPR8sx66/jHRY6cAraA0ngyAp5KN\nMAqeIfD7LvzVM9sakqxO0QIAdxPrCzlcUxx1tjxtOJ5hheOVTzhDBcsJWT9PscNi\nlP8w9EaSeg4+ZGRRlcFNx9EBe6M+Ov7mbuKoigfnXKgza+GXeJOYIGfdhh9XiFgd\nmfI59VPOxsaDzywlAL12eWZczaK2lC5cyk6IKg2zoWrPf13MtIG1iREKaxmOJbVp\nJwIDAQAB\n-----END PUBLIC KEY----'
+server_public_key = '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnJdl0MKhHQt8T9h1hefO\nl+DdBBP1kudIIq+aH7HpBV6aBoIqv0MgsNvIzEGtYF52anvAL7A8bWHulcKibP26\nyx5Jop0liBCZPvl53g4JqSJvhRscVMW0FojpifhJTuiytPbJOACy/Um2DJZVJbnm\nm+40+QUcW61QLfRAKsNthmKjyeX9qAPJhImwie2GHNRDByMCWXDlnb/VS6XTgsZG\nw+2dqOAX7TRIDGAhumZ6RgJY298fxj5XUdQ4vXp9tPc7D1MhIhVR/s/AY/uIVpPg\nYfbzAZ9jgpMEsLckNcr3n6OstOFyMGtPWrRiPzL/SCjl3QkveGuWHAbabu+H9qX7\nKQIDAQAB\n-----END PUBLIC KEY-----'
+client_private_key = ''
+
+import socket
+import sys
+from crypto import encrypt_RSA, decrypt_RSA
+import time
+import ipgetter
+import cPickle as pickle
+import ipaddr
+import socket, struct
+from netaddr import *
+
+def talkToServer(typeOfRequest, user, server, portToOpen, otp, clientPteKey):
+    global SERVER, KNOCK_PORT, s, msg, d, data, addr, reply
+    SERVER = server
+    KNOCK_PORT = 8888
+    nounce = int(time.time())
+    publicIp = ipgetter.myip()
+    ipaddrOfClient = socket.gethostbyname(socket.getfqdn())
+    client_private_key = clientPteKey
+
+    ip = IPAddress(ipaddrOfClient)
+    if (not (ip.is_unicast() and not ip.is_private())):
+        ipaddrOfClient = IPAddress(publicIp)
+
+
+    # Datagram (udp) socket
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        print 'Socket created'
+    except socket.error, msg:
+        print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        #sys.exit()
+        return False
+
+    # Bind socket to local host and port
+    try:
+        s.bind((SERVER, KNOCK_PORT))
+    except socket.error, msg:
+        print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        #sys.exit()
+        return False
+
+    print 'Socket bind complete'
+
+    #first comms
+    payload = [user, ipaddrOfClient, portToOpen, otp, nounce];
+    msg = raw_input(pickle(payload))  #serialize the payload
+    msg = encrypt_RSA(server_public_key, msg)
+    s.sendto(msg, (SERVER, KNOCK_PORT))
+
+    #second comms
+    # receive data from server (data, addr)
+    d = s.recvfrom(1024)
+    data = d[0]
+    addr = d[1]
+    data = decrypt_RSA(server_public_key, data)
+
+    if not (data == nounce):  #not fresh
+        return False
+
+    #third comms
+    reply = nounce
+    reply = encrypt_RSA(client_private_key, reply)  #this complete two way identity proofing
+    s.sendto(reply, addr)
+    print 'Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + data.strip()
+    s.close()
+    return True;
+
+talkToServer()
